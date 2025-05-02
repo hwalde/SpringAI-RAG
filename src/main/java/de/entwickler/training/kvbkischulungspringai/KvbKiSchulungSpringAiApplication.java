92301dfc9e270b00d5612fc09e4998f928865a2f
Herbert Walde <info@herbert-walde.de>
2025-05-02 16:08:45 +0200

    Preparation

---
+++ src/main/java/de/entwickler/training/kvbkischulungspringai/KvbKiSchulungSpringAiApplication.java
@@ -0,0 +1,13 @@
+package de.entwickler.training.kvbkischulungspringai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KvbKiSchulungSpringAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KvbKiSchulungSpringAiApplication.class, args);
+    }
+
+}
